21787427863cfd635528ad73a15d9a0f512381b2
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2024-05-16 19:12:24 +0200

    feat: landing page

---
+++ src/components/home/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Home.css";
+import plan_wat from "./plan_wat.jpg";
+
+function Home({ text }) {
+  return (
+    <div className="home">
+      <div className="home_top">
+        <div className="home_top_about">O projekcie</div>
+      </div>
+      <div className="home_bottom">
+        <div className="home_left">
+          <img className="plan_wat" src={plan_wat} alt="logo" />
+        </div>
+
+        <div className="home_right">
+          <div className="home_right_title">GEOPORTAL</div>
+
+          <div className="home_right_subtitle">
+            Strona internetowa z interaktywną mapą
+          </div>
+
+          <button className="home_right_button">START</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
