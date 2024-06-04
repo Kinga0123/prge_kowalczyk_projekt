bd8eb8ae22283a334c3e411ffd3aaab95ee6869f
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2024-06-04 19:08:32 +0200

    wip

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,9 +1,30 @@
-import React from "react";
-import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect } from "react";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+} from "react-leaflet";
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
 
 function Map() {
+  useEffect(() => {
+    console.log("aaa");
+    const getData = () => {
+      axios
+        .get(
+          // "http://127.0.0.1:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Agranice_wojewodztw_db&maxFeatures=50&outputFormat=application%2Fjson"
+          "https://jsonplaceholder.typicode.com/posts/1"
+        )
+        .then((dane) => {
+          console.log(dane);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <MapContainer center={[52.232222, 21.0]} zoom={6}>
@@ -11,12 +32,23 @@ function Map() {
           <LayersControl.BaseLayer checked name="OSM">
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="GoogleSatellite">
+          <LayersControl.BaseLayer name="GoogleSatelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
           <LayersControl.BaseLayer name="OpenTopoMap">
             <TileLayer url="https://tile.opentopomap.org/{z}/{x}/{y}.png" />
           </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="granice wojewodztw db">
+            <WMSTileLayer
+              layers="granice_wojewodztw_db"
+              url="http://127.0.0.1:8080/geoserver/prge/wms"
+            />
+          </LayersControl.BaseLayer>
+          {/* <LayersControl.Overlay name="granice_wojewodztwa_db_wfs">
+            <GeoJSON
+              data={}
+            />
+          </LayersControl.Overlay> */}
         </LayersControl>
       </MapContainer>
     </div>
