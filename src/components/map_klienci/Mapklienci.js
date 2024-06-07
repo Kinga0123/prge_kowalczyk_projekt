ef60a6b33f854406ad4f53c10d1800628db5bc01
Kinga0123 <149382131+Kinga0123@users.noreply.github.com>
2024-06-07 12:31:57 +0200

    wip: add wfs management

--- src/components/map_klienci/Mapklienci.js
+++ src/components/map_klienci/Mapklienci.js
@@ -1,26 +1,72 @@
-import React from "react";
-import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useState } from "react";
 import "./Mapklienci.css";
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+  GeoJSON,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import axios from "axios";
 
-function Mapklienci() {
+function Map() {
+  const [wojewodztwa, setwojewodztwa] = useState(null);
+
+  const makePopup = (feature, layer) => {
+    if (feature.properties) {
+      // console.log(feature.properties);
+      layer.bindPopup(`
+      <h1>Dane województwa</h1>
+      <strong>Nazwa:</strong> ${feature.properties.jpt_nazwa_}</br>
+      <strong>Powierzchnia:</strong>${feature.properties.jpt_powier} m2 </br>
+      <img src=${feature.properties.img_source} alt="Lamp" width="32" height="32"/>`);
+    }
+  };
+
+  useEffect(() => {
+    // console.log("aaa");
+    const getData = () => {
+      axios
+        .get(
+          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Agranice_wojewodztw_db_4326&maxFeatures=50&outputFormat=application%2Fjson"
+        )
+        .then((dane) => {
+          // console.log(dane);
+          setwojewodztwa(dane.data);
+        });
+    };
+    getData();
+  }, []);
   return (
-    <div>
-      <MapContainer center={[52.232222, 21.0]} zoom={6}>
+    <div className="map">
+      <MapContainer center={[52.2322222, 21.0]} zoom={10}>
         <LayersControl>
-          <LayersControl.BaseLayer checked name="OSM">
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <LayersControl.BaseLayer name="OSM">
+            <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="GoogleSatellite">
+          <LayersControl.BaseLayer name="Google">
+            <TileLayer url="http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}" />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Google Satelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="OpenTopoMap">
-            <TileLayer url="https://tile.opentopomap.org/{z}/{x}/{y}.png" />
+          <LayersControl.BaseLayer name="Granice województw DB">
+            <WMSTileLayer
+              layers="granice_wojewodztw"
+              url="http://127.0.0.1:8080/geoserver/prge/wms"
+            />
           </LayersControl.BaseLayer>
+          <LayersControl.Overlay checked name="Granice województw DB WFS">
+            {wojewodztwa ? (
+              <GeoJSON data={wojewodztwa} onEachFeature={makePopup} />
+            ) : (
+              ""
+            )}
+          </LayersControl.Overlay>
         </LayersControl>
       </MapContainer>
     </div>
   );
 }
-
-export default Mapklienci;
+export default Map;
